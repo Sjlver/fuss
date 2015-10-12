326c6c77a50b36cdffe3d67c08543e164ac5710e
Jonas Wagner <jonas.wagner@epfl.ch>
2015-10-12 16:52:02 +0200

    Add TSan to scripts/bzip2/build_bzip2

--- asap/scripts/bzip2/build_bzip2.sh
+++ asap/scripts/bzip2/build_bzip2.sh
@@ -11,6 +11,9 @@ ASAN_LDFLAGS="-fsanitize=address"
 UBSAN_CFLAGS="-fsanitize=undefined -fno-sanitize=shift -fno-sanitize-recover=all"
 UBSAN_LDFLAGS="-fsanitize=undefined"
 
+TSAN_CFLAGS="-fsanitize=thread"
+TSAN_LDFLAGS="-fsanitize=thread"
+
 
 # Fix Mac machines that don't have sha256sum
 if ! which sha256sum >/dev/null 2>&1; then
@@ -35,7 +38,7 @@ build_bzip2() {
     local extra_cflags="$1"
     local ldflags="$2"
     make clean
-    make -j "$N_PROCESSORS" \
+    make -j "$N_JOBS" \
          CC="$(which asap-clang)" \
          CFLAGS="-Wall -Winline -O3 -g -D_FILE_OFFSET_BITS=64 $extra_cflags" \
          LDFLAGS="$ldflags" \
@@ -52,9 +55,11 @@ fetch_bzip2
 build_asap_initial "bzip2" "baseline" "configure_and_build_bzip2" "" ""
 build_asap_initial "bzip2" "asan"     "configure_and_build_bzip2" "$ASAN_CFLAGS" "$ASAN_LDFLAGS"
 build_asap_initial "bzip2" "ubsan"    "configure_and_build_bzip2" "$UBSAN_CFLAGS" "$UBSAN_LDFLAGS"
+build_asap_initial "bzip2" "tsan"     "configure_and_build_bzip2" "$TSAN_CFLAGS" "$TSAN_LDFLAGS"
 
 build_asap_coverage "bzip2" "asan"  "build_bzip2" "$ASAN_CFLAGS" "$ASAN_LDFLAGS"
 build_asap_coverage "bzip2" "ubsan" "build_bzip2" "$UBSAN_CFLAGS" "$UBSAN_LDFLAGS"
+build_asap_coverage "bzip2" "tsan"  "build_bzip2" "$TSAN_CFLAGS" "$TSAN_LDFLAGS"
 
 build_asap_optimized "bzip2" "asan" "s0000" "-asap-sanity-level=0.000" "build_bzip2" "$ASAN_CFLAGS" "$ASAN_LDFLAGS"
 build_asap_optimized "bzip2" "asan" "c0010" "-asap-cost-level=0.010"   "build_bzip2" "$ASAN_CFLAGS" "$ASAN_LDFLAGS"
@@ -65,3 +70,8 @@ build_asap_optimized "bzip2" "ubsan" "s0000" "-asap-sanity-level=0.000" "build_b
 build_asap_optimized "bzip2" "ubsan" "c0010" "-asap-cost-level=0.010"   "build_bzip2" "$UBSAN_CFLAGS" "$UBSAN_LDFLAGS"
 build_asap_optimized "bzip2" "ubsan" "c0040" "-asap-cost-level=0.040"   "build_bzip2" "$UBSAN_CFLAGS" "$UBSAN_LDFLAGS"
 build_asap_optimized "bzip2" "ubsan" "c1000" "-asap-cost-level=1.000"   "build_bzip2" "$UBSAN_CFLAGS" "$UBSAN_LDFLAGS"
+
+build_asap_optimized "bzip2" "tsan" "s0000" "-asap-sanity-level=0.000" "build_bzip2" "$TSAN_CFLAGS" "$TSAN_LDFLAGS"
+build_asap_optimized "bzip2" "tsan" "c0010" "-asap-cost-level=0.010" "build_bzip2" "$TSAN_CFLAGS" "$TSAN_LDFLAGS"
+build_asap_optimized "bzip2" "tsan" "c0040" "-asap-cost-level=0.040" "build_bzip2" "$TSAN_CFLAGS" "$TSAN_LDFLAGS"
+build_asap_optimized "bzip2" "tsan" "c1000" "-asap-cost-level=1.000" "build_bzip2" "$TSAN_CFLAGS" "$TSAN_LDFLAGS"
