acacc9a019bd57c18aa72bba6acf2b80a77ee1c8
Jonas Wagner <jonas.wagner@epfl.ch>
2016-05-22 07:18:56 +0200

    A first version of SanityCheckSampledCostPass.
    This pass estimates instruction execution counts from block frequencies.

---
+++ lib/Transforms/SanityChecks/SanityCheckSampledCostPass.cpp
@@ -0,0 +1,135 @@
+// This file is part of ASAP.
+// Please see LICENSE.txt for copyright and licensing information.
+
+#include "SanityCheckSampledCostPass.h"
+#include "SanityCheckInstructionsPass.h"
+#include "CostModel.h"
+#include "utils.h"
+
+#include "llvm/Analysis/BlockFrequencyInfo.h"
+#include "llvm/Analysis/TargetTransformInfo.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Metadata.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/Format.h"
+
+#include <algorithm>
+#include <memory>
+#include <system_error>
+#define DEBUG_TYPE "sanity-check-cost"
+
+using namespace llvm;
+
+namespace {
+bool largerCost(const SanityCheckSampledCostPass::CheckCost &a,
+                const SanityCheckSampledCostPass::CheckCost &b) {
+  return a.second > b.second;
+}
+} // anonymous namespace
+
+bool SanityCheckSampledCostPass::runOnFunction(Function &F) {
+  DEBUG(dbgs() << "SanityCheckSampledCostPass on " << F.getName() << "\n");
+  CheckCosts.clear();
+
+  TargetTransformInfoWrapperPass &TTIWP =
+      getAnalysis<TargetTransformInfoWrapperPass>();
+
+  const TargetTransformInfo &TTI = TTIWP.getTTI(F);
+  SanityCheckInstructionsPass &SCI = getAnalysis<SanityCheckInstructionsPass>();
+  BlockFrequencyInfo &BFI = getAnalysis<BlockFrequencyInfo>();
+
+  for (Instruction *Inst : SCI.getSanityCheckRoots()) {
+    assert(Inst->getParent()->getParent() == &F &&
+           "SCI must only contain instructions of the current function.");
+
+#ifndef NDEBUG
+    int nInstructions = 0;
+    int nFreeInstructions = 0;
+    uint64_t maxCount = 0;
+#endif
+
+    // The cost of a check is the sum of the cost of all instructions
+    // that this check uses.
+    // TODO: If an instruction is used by multiple checks, we need an
+    // intelligent way to handle the nonlinearity.
+    double Cost = 0;
+    for (Instruction *CI : SCI.getInstructionsBySanityCheck(Inst)) {
+      unsigned CurrentCost = sanitychecks::getInstructionCost(CI, &TTI);
+
+      // Assume a default cost of 1 for unknown instructions
+      if (CurrentCost == (unsigned)(-1)) {
+        CurrentCost = 1;
+      }
+
+      DEBUG(if (CurrentCost == 0) { nFreeInstructions += 1; });
+
+      assert(CurrentCost <= 100 && "Outlier cost value?");
+
+      Cost += CurrentCost * getExecutionCount(CI, BFI);
+      DEBUG(nInstructions += 1);
+      DEBUG(
+          if (getExecutionCount(CI, BFI) > maxCount) { maxCount = getExecutionCount(CI, BFI); });
+    }
+
+    APInt CountInt = APInt(64, (uint64_t)Cost);
+    MDNode *MD = MDNode::get(
+        F.getContext(), {ConstantAsMetadata::get(ConstantInt::get(
+                            Type::getInt64Ty(F.getContext()), CountInt))});
+    Inst->setMetadata("cost", MD);
+    CheckCosts.push_back(std::make_pair(Inst, (uint64_t)Cost));
+
+    DEBUG(dbgs() << "Sanity check: " << *Inst << "\n";
+          DebugLoc DL = getInstrumentationDebugLoc(Inst);
+          printDebugLoc(DL, F.getContext(), dbgs());
+          dbgs() << "\nnInstructions: " << nInstructions << "\n";
+          dbgs() << "nFreeInstructions: " << nFreeInstructions << "\n";
+          dbgs() << "maxCount: " << maxCount << "\n";
+          dbgs() << "Cost: " << Cost << "\n";);
+  }
+
+  std::sort(CheckCosts.begin(), CheckCosts.end(), largerCost);
+
+  return false;
+}
+
+void SanityCheckSampledCostPass::getAnalysisUsage(AnalysisUsage &AU) const {
+  AU.addRequired<BlockFrequencyInfo>();
+  AU.addRequired<TargetTransformInfoWrapperPass>();
+  AU.addRequired<SanityCheckInstructionsPass>();
+  AU.setPreservesAll();
+}
+
+void SanityCheckSampledCostPass::print(raw_ostream &O, const Module *M) const {
+  O << "                Cost Location\n";
+  for (const CheckCost &I : CheckCosts) {
+    O << format("%20llu ", I.second);
+    DebugLoc DL = getInstrumentationDebugLoc(I.first);
+    printDebugLoc(DL, M->getContext(), O);
+    O << '\n';
+  }
+}
+
+double SanityCheckSampledCostPass::getExecutionCount(const Instruction *I, const BlockFrequencyInfo &BFI) const {
+  const BasicBlock *BB = I->getParent();
+  BlockFrequency Freq = BFI.getBlockFreq(BB);
+  uint64_t EntryFreq = BFI.getEntryFreq();
+
+  // Entry counts for functions are the number of samples in the entry block.
+  // I'm not sure how precise this is and whether we should mess with it or
+  // not. Lots of functions have zeros, though, so let's assume they execute at
+  // least once.
+  Optional<uint64_t> EntryCount = BB->getParent()->getEntryCount();
+  uint64_t AdjustedCount = EntryCount.hasValue() && EntryCount.getValue() > 0
+    ? EntryCount.getValue() : 1;
+
+  double scale = (double)Freq.getFrequency() / EntryFreq;
+  return AdjustedCount * scale;
+}
+
+char SanityCheckSampledCostPass::ID = 0;
+static RegisterPass<SanityCheckSampledCostPass>
+    X("sanity-check-sampled-cost", "Finds costs of sanity checks", false, false);
---
+++ lib/Transforms/SanityChecks/SanityCheckSampledCostPass.h
@@ -0,0 +1,37 @@
+// This file is part of ASAP.
+// Please see LICENSE.txt for copyright and licensing information.
+
+#include "llvm/Pass.h"
+
+#include <utility>
+#include <vector>
+
+namespace llvm {
+class BlockFrequencyInfo;
+class Instruction;
+class raw_ostream;
+}
+
+struct SanityCheckSampledCostPass : public llvm::FunctionPass {
+  static char ID;
+
+  SanityCheckSampledCostPass() : FunctionPass(ID) {}
+
+  virtual bool runOnFunction(llvm::Function &F);
+
+  virtual void getAnalysisUsage(llvm::AnalysisUsage &AU) const;
+
+  virtual void print(llvm::raw_ostream &O, const llvm::Module *M) const;
+
+  // A pair that stores a sanity check and its cost.
+  typedef std::pair<llvm::Instruction *, uint64_t> CheckCost;
+
+  const std::vector<CheckCost> &getCheckCosts() const { return CheckCosts; };
+
+private:
+  // Checks in the current function, with their cost.
+  std::vector<CheckCost> CheckCosts;
+
+  // Estimates the execution count for the given instruction
+  double getExecutionCount(const llvm::Instruction *I, const llvm::BlockFrequencyInfo &BFI) const;
+};
