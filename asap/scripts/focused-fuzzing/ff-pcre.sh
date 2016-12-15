bbf3ba0ddd410a425206b3e7df86e42c6c8107df
Alexandra Sandulescu <alecsandra.sandulescu@gmail.com>
2016-12-15 12:14:26 +0200

    [pcre ff] fix inavalid download url; fix configure cflags

--- asap/scripts/focused-fuzzing/ff-pcre.sh
+++ asap/scripts/focused-fuzzing/ff-pcre.sh
@@ -1,7 +1,8 @@
+SRC_DIR="pcre2-10.22"
 init_target() {
-  if ! [ -d pcre2-10.20 ]; then
-    wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-10.20.tar.gz
-    tar xf pcre2-10.20.tar.gz
+  if ! [ -d $SRC_DIR ]; then
+    wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-10.22.tar.gz
+    tar xf pcre2-10.22.tar.gz
   fi
 }
 
@@ -18,12 +19,12 @@ build_target_and_fuzzer() {
     # Configure pcre2 with relatively low resource limits, to ensure fuzzing is
     # fast, and to avoid false positives from AddressSanitizer's low stack
     # overflow limits.
-    CC="$CC" CXX="$CXX" CFLAGS="$DEFAULT_CFLAGS $extra_cflags" LDFLAGS="$DEFAULT_LDFLAGS $extra_ldflags" ../pcre2-10.20/configure \
+    CC="$CC" CXX="$CXX" CFLAGS="$DEFAULT_CFLAGS" LDFLAGS="$DEFAULT_LDFLAGS" ../$SRC_DIR/configure \
       --disable-shared --with-parens-nest-limit=200 --with-match-limit=1000000 --with-match-limit-recursion=200
-    make -j $N_CORES V=1 libpcre2-posix.la libpcre2-8.la 2>&1 | tee "../logs/build-${name}.log"
+    make CFLAGS="$DEFAULT_CFLAGS $extra_cflags" LDFLAGS="$DEFAULT_LDFLAGS $extra_ldflags"  -j $N_CORES V=1 libpcre2-posix.la libpcre2-8.la 2>&1 | tee "../logs/build-${name}.log"
 
     "$CXX" $DEFAULT_CFLAGS $extra_cflags -std=c++11 \
-      -I "$WORK_DIR/target-${name}-build/src" -I "$WORK_DIR/pcre2-10.20/src" \
+      -I "$WORK_DIR/target-${name}-build/src" -I "$WORK_DIR/$SRC_DIR/src" \
       -c "$SCRIPT_DIR/ff-pcre.cc"
     "$CXX" $DEFAULT_LDFLAGS $extra_ldflags ff-pcre.o \
       "$WORK_DIR/target-${name}-build/.libs/libpcre2-posix.a" \
