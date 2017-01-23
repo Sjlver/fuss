6ff47a46c4e7f57d03f61c34ea4afff080e1f5ab
Jonas Wagner <jonas.wagner@epfl.ch>
2017-01-23 16:22:16 +0000

    Annotate.py: Fix typo in regular expression.

--- asap/scripts/annotate/annotate.py
+++ asap/scripts/annotate/annotate.py
@@ -25,7 +25,7 @@ class Annotator(object):
     FUNCTION_RE = re.compile(r'^AsapPass: ran on (\w+) at ([^:]+):(\d+)',
             re.MULTILINE)
     # /path/file.c:560:11:2: asap action:keeping cost:0 type:__sanitizer_cov_trace_pc_guard inlined:/path/file.c:1117:13:0
-    CHECK_RE = re.compile(r'^([^:\n]+):(\d+):(\d+):\d\+ asap action:(keeping|removing) cost:(\d+)(?: type:(\w+))?',
+    CHECK_RE = re.compile(r'^([^:\n]+):(\d+):(\d+):\d+: asap action:(keeping|removing) cost:(\d+)(?: type:(\w+))?',
             re.MULTILINE)
 
 
