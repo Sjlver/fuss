e9a20b5018f6cbe6fe49e8532c434cb4b03256a1
Jonas Wagner <jonas.wagner@epfl.ch>
2017-01-26 17:56:49 +0000

    Trying to improve coverage plots. Well...

--- asap/scripts/focused-fuzzing/speed_and_coverage_plots_all.py
+++ asap/scripts/focused-fuzzing/speed_and_coverage_plots_all.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-DATA_FILE_RE = re.compile(r'(?:.*/)?([^/]+)/logs/.*\.log')
+DATA_FILE_RE = re.compile(r'(?:.*/)?([^/]+)/logs/.*-(\d+)\.log')
 
 BENCHMARK_NAMES_MAP = {
     'ff-boringssl-2016-02-12': 'boringssl',
@@ -44,9 +44,14 @@ def load_data(data_files):
         if not m:
             raise ValueError("Unexpected data file name: " + data_file)
         cur_data = pd.read_table(data_file, sep=r'\s*\t\s*', engine='python')
-        cur_data['benchmark'] = m.group(1)
+        cur_data['benchmark'] = BENCHMARK_NAMES_MAP[m.group(1)]
+        cur_data['name'] = cur_data['name'].map(lambda n: re.sub('-\d\d$', '', n))
+        cur_data['run_id'] = int(m.group(2))
+        cur_data.set_index(['benchmark', 'name', 'run_id'], inplace=True)
+        cur_data['execs'] = normalize_execs(cur_data['execs'])
+        cur_data['actual_cov'] = normalize_coverage(cur_data['actual_cov'])
         data.append(cur_data)
-    data = pd.concat(data, ignore_index=True)
+    data = pd.concat(data)
     return data
 
 def bootstrap_ci(s, f, confidence=0.95):
@@ -64,27 +69,48 @@ def bootstrap_ci(s, f, confidence=0.95):
             ((1 - confidence) * 50, 100 - (1 - confidence) * 50))
     return pd.Series({'estimate': estimate, 'lower': lower, 'upper': upper})
 
+def normalize_execs(execs):
+    """Normalize executions; scale them to the noinstr version."""
+    return execs / float(execs.loc[:, 'noinstr', :])
 
+def normalize_coverage(cov):
+    """Compute coverage deltas"""
+
+    # FIXME: normalization (i.e., subtracting noinstr coverage) is not quite
+    # correct for asan, because we don't know the initial coverage in this
+    # case. ASan and TPCG use different initial corpora for the moment.
+    return np.maximum(0, cov - float(cov.loc[:, 'noinstr', :]))
+
+def normalize_aggregated_coverage(cov):
+    """Normalize coverage; scale them relative to no-fuss version."""
+
+    factors = {}
+    for version in cov.index.levels[1]:
+        base = re.sub(r'-.*', '', version)
+        base = base.replace('noinstr', 'tpcg')
+        factors[version] = float(cov.loc[:, base, 'estimate'])
+    factors = pd.Series(factors)
+    cov = cov.div(factors, level=1)
+    return cov
 
 def main():
     parser = argparse.ArgumentParser(description="Plots graphs for speed and coverage.")
-    parser.add_argument("--output", help="Output file to save plot to")
+    parser.add_argument("--output-execs", help="Output file to save execs plot to")
+    parser.add_argument("--output-cov", help="Output file to save cov plot to")
     parser.add_argument("data", nargs="+")
     args = parser.parse_args()
 
     data = load_data(args.data)
-    data['name'] = data['name'].map(lambda n: re.sub('-\d\d$', '', n))
-    data['benchmark'] = data['benchmark'].map(BENCHMARK_NAMES_MAP)
 
-    grouped = data.groupby(['benchmark', 'name'])
-    medians = grouped['execs'].apply(lambda s: bootstrap_ci(s, np.median))
-    medians = medians.groupby(level='benchmark').transform(lambda x: x / x.loc[:, 'noinstr', 'estimate'])
-
-    fig, ax = plt.subplots()
-    benchmarks = medians.index.levels[0]
-    bar_width = 1.0 / (len(VERSIONS) + 2)
+    grouped = data.groupby(level=['benchmark', 'name'])
+    benchmarks = data.index.levels[0]
     xs = np.arange(len(benchmarks))
-    
+    bar_width = 1.0 / (len(VERSIONS) + 2)
+
+    # Plot median executions
+    medians = grouped['execs'].apply(lambda s: bootstrap_ci(s, np.median))
+
+    fig = plt.figure()
     for i, version in enumerate(VERSIONS):
         plt.bar(xs + (i + 1) * bar_width,
                 medians.loc[:, version, 'estimate'],
@@ -100,9 +126,35 @@ def main():
     plt.xticks(xs + (len(VERSIONS) / 2 + 1) * bar_width, benchmarks)
     plt.grid(True, axis='y')
     
-    fig.set_size_inches(15, 5)
-    if args.output:
-        plt.savefig(args.output)
+    fig.set_size_inches(12, 4)
+    if args.output_execs:
+        plt.savefig(args.output_execs)
+    else:
+        plt.show()
+
+    # A similar plot for coverage increase instead of executions
+    medians = grouped['actual_cov'].apply(lambda s: bootstrap_ci(s, np.median))
+    medians = medians.groupby(level='benchmark').transform(normalize_aggregated_coverage)
+
+    fig = plt.figure()
+    for i, version in enumerate(VERSIONS):
+        plt.bar(xs + (i + 1) * bar_width,
+                medians.loc[:, version, 'estimate'],
+                bar_width,
+                color=COLORS[version],
+                yerr=(medians.loc[:, version, 'estimate'] - medians.loc[:, version, 'lower'],
+                    medians.loc[:, version, 'upper'] - medians.loc[:, version, 'estimate']),
+                label=version)
+
+    plt.xlabel("Benchmark")
+    plt.xlim(0, len(benchmarks))
+    plt.ylabel("Coverage (BB, rel. to baseline)")
+    plt.xticks(xs + (len(VERSIONS) / 2 + 1) * bar_width, benchmarks)
+    plt.grid(True, axis='y')
+    
+    fig.set_size_inches(12, 4)
+    if args.output_cov:
+        plt.savefig(args.output_cov)
     else:
         plt.show()
     
